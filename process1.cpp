1f592350a34b57eb10a4bf3d63802c4ec3b7c01d
M-sravan-kumar <42943339+M-sravan-kumar@users.noreply.github.com>
2019-04-17 20:35:48 +0530

    Add files via upload

--- process1.cpp
+++ process1.cpp
@@ -5,58 +5,82 @@ struct proc
 	int worsttime;
 	int arrivaltime;
 	int ct;
-};
+}*ptr,*max=NULL,*secondmax=NULL;
 
 
+//void fun()
 
 
 int main()
 {
-	int n;
-	printf("enter how many process u want to enter\n");
-	scanf("%d",&n);
-	proc p[n];
-	for(int i=0;i<n;i++)
+	proc p[3];
+	printf("enter 3 process u want to enter\n");
+	for(int i=0;i<3;i++)
 	{
 		printf("enter %d pocess id\n",i+1);
 		scanf("%d",&p[i].processid);
 		printf("enter worsttime for 1st process\n");
-		scanf("%d",&p[i].bursttime);
-		printf("enter arrival time\n");
-		scanf("%d",&p[i].arrivaltime);
-	}
+		scanf("%d",&p[i].worsttime);
+	} 
 	
-	while(p[0]!=0 && p[1]!=0 && p[2]!=0)
-	{ 
-     
-    proc max=p[0];
-	int l=p.length;
-	for(int i=0;i<l;i++)
+	do
 	{
-	if(max.worsttime<p[i].worsttime)
-	{
-	max=p[i];
-    }
-	}
-	
-	for(i=0;i<n;i++)
-	{
-		if(max==p[i])
+		if(p[0].worsttime>=p[1].worsttime && p[0].worsttime>=p[2].worsttime)
 		{
-			p[i].worsttime--;
-			break;
+			if(p[1].worsttime > p[2].worsttime)
+			{
+				while(p[0].worsttime!=p[1].worsttime)
+				{
+					printf("%d",p[0].processid);
+					p[0].worsttime--;
+				}
+			}
+			else
+			{
+				while(p[0].worsttime!=p[2].worsttime)
+				{
+					printf("%d",p[0].processid);
+					p[0].worsttime--;
+				}
+			}
 		}
-	}
-	
-	for(i=0;i<n;i++)
-	{
-		if(max!=p[i].worsttime)
+		
+		else if(p[1].worsttime >= p[2].worsttime )
 		{
-			max.worsttime--;
+			if(p[1].worsttime==p[2].worsttime)
+			{
+				if(p[1].processid<p[2].processid)
+				{
+					printf("%d",p[1].processid);
+					p[1].worsttime--;
+				}
+				else
+				{
+					printf("%d",p[2].processid);
+					p[2].worsttime--;
+				}
+			}
+			while(p[1].worsttime!=p[2].worsttime)
+			{
+				printf("%d",p[1].processid);
+				p[1].worsttime--;
+			}
 		}
-	}
-	
-	
+		
+		else if(p[2].worsttime>p[1].worsttime)
+		{
+			while(p[2].worsttime!=p[1].worsttime)
+			{
+				printf("%d",p[2].processid);
+				p[2].worsttime--;
+			}
+		}
+}
+
+while(p[0].worsttime!=0 && p[1].worsttime!=0 && p[2].worsttime!=0);
+
+printf("%d",p[2].worsttime);
+		
 }
 
 
